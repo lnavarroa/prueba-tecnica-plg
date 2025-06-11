742989cd2364c894936f308d59324b68c138b9b8
Luis Navarro <luis.navarro.arenas@gmail.com>
2025-06-11 07:23:11 -0400

    Olvidé subir la base de datos

---
+++ Clientes_202506110721.sql
@@ -0,0 +1,15 @@
+INSERT INTO prueba_tecnica.dbo.Clientes (Nombre,Telefono,Pais) VALUES
+	 (N'Luis Navarro',N'+569 1234 5678',N'Chile'),
+	 (N'Ana Martínez',N'+34 654 321 987',N'España'),
+	 (N'Carlos López',N'+57 312 456 7890',N'Colombia'),
+	 (N'Juan Pérez',N'56912345678',N'Chile'),
+	 (N'María González',N'56923456789',N'Argentina'),
+	 (N'Carlos Ramírez',N'56934567890',N'México'),
+	 (N'Ana Fernández',N'56945678901',N'Perú'),
+	 (N'Pedro Martínez',N'56956789012',N'Colombia'),
+	 (N'Sofía López',N'56967890123',N'España'),
+	 (N'Luis Torres',N'56978901234',N'Chile');
+INSERT INTO prueba_tecnica.dbo.Clientes (Nombre,Telefono,Pais) VALUES
+	 (N'Camila Rojas',N'56989012345',N'Argentina'),
+	 (N'Jorge Castillo',N'56990123456',N'México'),
+	 (N'Isabel Reyes',N'56901234567',N'Perú');
